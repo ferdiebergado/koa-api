e7492d41ccae9f96ff7af876f8e09409c08396ba
ferdie <ferdiebergado@gmail.com>
2020-02-25 12:45:39 +0800

    Refactored to es5 function for jsdoc autocompletion

--- src/middlewares/validationMiddleware.js
+++ src/middlewares/validationMiddleware.js
@@ -1,23 +1,32 @@
 /* eslint-disable no-useless-catch */
-module.exports = {
-  // Validates the request body/route parameter based on the supplied schema
-  validate: (schema, param) => {
-    return async (ctx, next) => {
+/**
+ * Validate the request body/route parameter based on the supplied schema
+ *
+ * @param {Object} schema - The validation schema
+ * @param {string} param - If set, validates the route parameter with the set name
+ */
+function validate(schema, param = null) {
+  return async (ctx, next) => {
+    try {
       // Default to validating the request body
       let request = ctx.request.body;
 
-      // If param argument is present, validate the route parameter
+      // If param argument is set, validate the route parameter
       // eslint-disable-next-line security/detect-object-injection
       if (param) request = { [param]: ctx.params[param] };
 
-      try {
-        await schema.validateAsync(request, {
-          abortEarly: false
-        });
-        await next();
-      } catch (error) {
-        throw error;
-      }
-    };
-  }
-};
+      // Validate
+      await schema.validateAsync(request, {
+        abortEarly: false
+      });
+
+      // Call the next route handler
+      await next();
+    } catch (error) {
+      // Bubble up the error to the global error handler
+      throw error;
+    }
+  };
+}
+
+module.exports = { validate };
